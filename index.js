54d3b47ce2274c2f9d396f7b9b6a616cf69d2693
Bilal <tahabilal@vt.edu>
2020-07-04 21:01:12 -0400

    Commit 2

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const app = express(); // Generates a running application that represents a sing
 // Arrow function is called automatically when a single request comes in
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
-  console.log(req);
 });
 
 // Whenever Heroku/etc has the option to pass environment variables, it will look for these variables
 // If there is no environment variable, then use 5000 (for development)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//https://rugged-hot-springs-35104.herokuapp.com/ | https://git.heroku.com/rugged-hot-springs-35104.git
